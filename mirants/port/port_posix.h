afa603a9e1e155531a9dc9478895a1c2225b2cf5
QiumingLu <cming_cs@163.com>
2015-11-21 22:41:26 +0800

    posix

---
+++ mirants/port/port_posix.h
@@ -0,0 +1,50 @@
+#ifndef MIRANTS_PORT_PORT_POSIX_H
+#define MIRANTS_PORT_PORT_POSIX_H
+
+#include <pthread.h>
+
+namespace mirants {
+namespace port {
+
+class CondVar;
+
+class Mutex {
+ public:
+  Mutex();
+  ~Mutex();
+
+  void Lock();
+  void Unlock();
+  void AssertHeld() { }
+
+ private:
+  friend class CondVar;
+  pthread_mutex_t mu_;
+
+  // No copying
+  Mutex(const Mutex&);
+  void operator=(const Mutex&);
+};
+
+class CondVar {
+ public:
+  explicit CondVar(Mutex* mu);
+  ~CondVar();
+
+  void Wait();
+  void Signal();
+  void SignalAll();
+
+ private:
+  pthread_cond_t cv_;
+  Mutex* mu_;
+};
+
+typedef pthread_once_t OnceType;
+#define MIRANTS_ONCE_INIT PTHREAD_ONCE_INIT
+extern void InitOnce(OnceType* once, void (*initializer)());
+
+} // namespace port
+} // namespace mirants
+
+#endif  // MIRANTS_PORT_PORT_POSIX_H
